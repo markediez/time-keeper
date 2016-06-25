cae81b7aa86770753d675a312112804369432b08
Mark Diez <markediez@gmail.com>
2016-06-25 15:33:58 -0700

    Update control panel design

--- server.php
+++ server.php
@@ -5,6 +5,8 @@ session_start();
 function addHeaders($title) {
   echo "<title>$title</title>";
   echo '<meta charset="utf-8">';
+  echo "<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>";
+  echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
   echo '<link rel="stylesheet" href="http://' . $_SERVER['HTTP_HOST'] . '/vendor/bootstrap-3.3.6-dist/css/bootstrap.min.css">';
   echo '<link rel="stylesheet" href="http://' . $_SERVER['HTTP_HOST'] . '/vendor/font-awesome-4.6.3/css/font-awesome.min.css">';
   echo '<link rel="stylesheet" href="http://' . $_SERVER['HTTP_HOST'] . '/style.css">';
@@ -40,5 +42,17 @@ function redirect($url, $statusCode = 303) {
   header('Location: ' . $url, true, $statusCode);
   die();
 }
+
+function addPanel() { ?>
+  <div id="logo" class="row">
+    <img src="svg/logo.svg" class="img-responsive col-md-4">
+    <span class="col-md-8">Time Keeper</span>
+  </div>
+  <div id="links" class="row">
+    <a href="time-keeper.php" class="col-md-12 btn btn-panel">Work</a>
+    <a href="worklog.php" class="col-md-12 btn btn-panel">Log</a>
+    <a href="logout.php" class="col-md-12 btn btn-panel">Logout</a>
+  </div>
+<?php }
 
 ?>
