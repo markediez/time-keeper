2681c5e55bbdcdb45e6ccd69a5123c8cb3478009
Mark Diez <markediez@gmail.com>
2016-06-25 15:34:27 -0700

    Finish calendar design

--- worklog.php
+++ worklog.php
@@ -25,108 +25,99 @@ $db = new DBLite();
 </head>
 <body>
   <div class="container-fluid">
-
-    <div id="header" class="row">
-      <div class="col-md-12">
-        <h1>Time Keeper</h1>
-        <a href="time-keeper.php" class="col-md-1">Work</a>
-        <a href="worklog.php" class="col-md-1">Log</a>
-        <a href="logout.php" class="col-md-1">Logout</a>
+    <div class="row">
+      <div id="panel" class="col-md-2">
+        <?
+          addPanel();
+        ?>
       </div>
-    </div>
 
-    <div id="log" class="row">
-      <div id="calendar" class="col-md-9 col-md-offset-1">
-        <!-- Month Title -->
-        <div class="week col-md-12 text-center">
-          <div class="month col-md-12"><span><?=$month?></span></div>
-        </div>
+      <div id="content" class="col-md-10">
+        <div id="calendar" class="col-md-12 no-padding">
+          <!-- Month Title -->
+          <div class="col-md-12 text-center month no-padding">
+            <span id="month-name"><?=$month?></span>
+          </div>
 
-        <!-- Day Title -->
-        <div class="week col-md-12">
-          <?php
-          $day=array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-          for ($i = 0; $i < 7; $i++) {
-            echo '<div class="day-head col-md-1 text-center">';
-            echo "<span>$day[$i]</span>";
-            echo '</div>';
-          }
-          ?>
-        </div>
-
-        <!-- Weeks -->
-        <?php
-        $dow =  $start_date->format('D');
-        $end_day = $end_date->format('d');
-        $start_day = 0;
-        while ($start_day < 7) {
-          if(strpos($day[$start_day], $dow) !== false) break;
-          $start_day++;
-        }
-        $start_date->setTime(0,0,0);
-        $end_date->setTime(0,0,0);
-        $start_date = $start_date->format('Y-m-d H:i:s');
-        $end_date = $end_date->format('Y-m-d H:i:s');
-        $query = "SELECT Jobs.id as job_id, Jobs.title as job_title, WorkLog.title as work_title, WorkLog.start_time, WorkLog.end_time FROM WorkLog INNER JOIN Jobs ON WorkLog.job_id = Jobs.id WHERE Worklog.user_id = :uid AND Worklog.start_time BETWEEN :sd AND :ed ORDER BY WorkLog.start_time ASC";
-        $statement = $db->prepare($query);
-        $statement->bindValue(':uid', $_SESSION['user_id']);
-        $statement->bindValue(':sd', $start_date);
-        $statement->bindValue(':ed', $end_date);
-        $res = $statement->execute();
-
-        $log = array();
-        $total_duration = array();
-        while($row = $res->fetchArray()) {
-          $day = strtotime($row['start_time']);
-          $day = date('d', $day);
-          $sd = new DateTime($row['start_time']);
-          $ed = new DateTime($row['end_time']);
-          $interval = $ed->diff($sd);
-          $duration = $interval->h + ($interval->i / 60);
-          if(!is_array($log[$day])) {
-            $log[$day] = array();
-          }
-
-          $event = array();
-          $event['job_title'] = $row['job_title'];
-          $event['work_title'] = $row['work_title'];
-          $event['duration'] = $duration;
-          array_push($log[$day], $event);
-          $total_duration[$row['job_id']] += $duration;
-        }
-        // Disabled days
-
-        for ($i = 1; $i <= 5; $i++) {
-          echo '<div class="week col-md-12">';
-          for ($j = 1; $j <= 7; $j++) {
-            $day_num = 7 * ($i - 1) + $j;
-            $dom = $day_num - $start_day;
-            if($dom <= 0 || $dom  > $end_day) {
-              echo '<div class="day col-md-1 no-padding disabled">';
-            } else {
-              echo '<div class="day col-md-1 no-padding">';
-              echo '<span class="event-date col-md-9 no-padding">' . ($day_num - $start_day) . '</span>';
+          <!-- Day Title -->
+          <div class="week col-md-12 no-padding">
+            <?php
+            $day=array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+            for ($i = 0; $i < 7; $i++) {
+              echo '<div class="day-head col-md-1 text-center">';
+              echo "<span>$day[$i]</span>";
+              echo '</div>';
             }
-            for ($event = 0; $event < sizeof($log[$dom]); $event++) {
-              echo '<div class="event col-md-12 no-padding">';
-              echo '<span class="col-md-8 ">' . $log[$dom][$event]['job_title'] . '</span>';
-              echo '<span class="col-md-4 ">' . $log[$dom][$event]['duration'] . '</span>';
+            ?>
+          </div>
+
+          <!-- Weeks -->
+          <?php
+          $dow =  $start_date->format('D'); // Wed
+          $end_day = $end_date->format('d');
+          $start_day = 0;
+          while ($start_day < 7) {
+            if(strpos($day[$start_day], $dow) !== false) break;
+            $start_day++;
+          }
+          $start_date->setTime(0,0,0);
+          $end_date->setTime(0,0,0);
+          $start_date = $start_date->format('Y-m-d H:i:s');
+          $end_date = $end_date->format('Y-m-d H:i:s');
+          $query = "SELECT Jobs.id as job_id, Jobs.title as job_title, WorkLog.title as work_title, WorkLog.start_time, WorkLog.end_time FROM WorkLog INNER JOIN Jobs ON WorkLog.job_id = Jobs.id WHERE Worklog.user_id = :uid AND Worklog.start_time BETWEEN :sd AND :ed ORDER BY WorkLog.start_time ASC";
+          $statement = $db->prepare($query);
+          $statement->bindValue(':uid', $_SESSION['user_id']);
+          $statement->bindValue(':sd', $start_date);
+          $statement->bindValue(':ed', $end_date);
+          $res = $statement->execute();
+
+          $log = array();
+          $total_duration = array();
+          while($row = $res->fetchArray()) {
+            $day = strtotime($row['start_time']);
+            $day = date('d', $day);
+            $sd = new DateTime($row['start_time']);
+            $ed = new DateTime($row['end_time']);
+            $interval = $ed->diff($sd);
+            $duration = $interval->h + ($interval->i / 60);
+            if(!is_array($log[$day])) {
+              $log[$day] = array();
+            }
+
+            $event = array();
+            $event['job_title'] = $row['job_title'];
+            $event['work_title'] = $row['work_title'];
+            $event['duration'] = $duration;
+            array_push($log[$day], $event);
+            $total_duration[$row['job_id']] += $duration;
+          }
+          // Disabled days
+
+          for ($i = 1; $i <= 5; $i++) {
+            echo '<div class="week col-md-12 no-padding">';
+            for ($j = 1; $j <= 7; $j++) {
+              $day_num = 7 * ($i - 1) + $j;
+              $dom = $day_num - $start_day;
+              if($dom <= 0 || $dom  > $end_day) {
+                echo '<div class="day col-md-1 no-padding disabled">';
+              } else {
+                echo '<div class="day col-md-1 no-padding">';
+                echo '<span class="event-date col-md-9 no-padding">' . ($day_num - $start_day) . '</span>';
+              }
+              for ($event = 0; $event < sizeof($log[$dom]); $event++) {
+                echo '<div class="event col-md-12 no-padding">';
+                echo '<span class="col-md-8 ">' . $log[$dom][$event]['job_title'] . '</span>';
+                echo '<span class="col-md-4 ">' . number_format((float)$log[$dom][$event]['duration'], 2) . '</span>';
+                echo '</div>';
+              }
               echo '</div>';
             }
             echo '</div>';
           }
-          echo '</div>';
-        }
-        ?>
-      </div> <!-- End Calendar -->
-      <div id="legend-container" class="col-md-2" style="background-color:purple; height: 100px;">
-        <?php
-          $query="";
-        ?>
-        <span>HI THERE</span>
-      </div>
-    </div> <!-- End Legend -->
-  </div> <!-- End Log -->
+          ?>
+        </div> <!-- End Calendar -->
+      </div> <!-- End Content -->
+    </div>
 </div>
 </body>
 </html>
