a3611027a169a172e77aa9d0829691d31db711d9
Mark Diez <markediez@gmail.com>
2016-07-06 21:12:42 -0700

    Finish skeleton design description

--- time-progress.php
+++ time-progress.php
@@ -50,8 +50,7 @@
 
         <div id="content" class="col-md-10">
 
-          <div id="clock" class="row">
-            <h2 class="col-md-12"><?= $title ?></h2>
+          <div id="clock" class="col-md-4">
             <div id="clock-bg" class="col-md-6 col-md-offset-3">
               <div id="clock-inner" class="flex">
                 <div id="clock-middle"></div>
@@ -60,44 +59,42 @@
             </div>
           </div> <!-- End Clock -->
 
-          <div id="description" class="col-md-10 col-md-offset-1">
+          <div id="description" class="col-md-8">
+            <h2 class="row title"><?= $title ?></h2>
             <div id="entries" class="row">
 
-              <div class="col-md-12 saved-entry flex">
-                <span class="col-md-1 flex flex-vertical-center flex-end">1.</span>
-                <input id="entry-1" type="text" class="col-md-10 entry" value="Added this" disabled>
-                <span class="col-md-1 flex flex-vertical-center flex-space-around">
+              <div class="col-md-12 saved-entry flex no-padding">
+                <span class="col-md-half flex flex-vertical-center flex-end no-padding">1.</span>
+                <input id="entry-1" type="text" class="col-md-10 entry " value="Added this" disabled>
+                <span class="col-md-1 flex flex-vertical-center flex-space-around no-padding">
                   <a onclick="toggleEntry('#entry-1')"><i class="fa fa-pencil " aria-hidden="true"></i></a>
                   <a onclick="deleteEntry('#entry-1')"><i class="fa fa-trash" aria-hidden="true"></i></a>
                 </span>
               </div>
 
-              <div class="col-md-12 saved-entry flex">
-                <span class="col-md-1 flex flex-vertical-center flex-end">2.</span>
-                <input id="entry-2" type="text" class="col-md-10 entry" value="Added this too" disabled>
-                <span class="col-md-1 flex flex-vertical-center flex-space-around">
+              <div class="col-md-12 saved-entry flex no-padding">
+                <span class="col-md-half flex flex-vertical-center flex-end no-padding">2.</span>
+                <input id="entry-1" type="text" class="col-md-10 entry " value="Added this" disabled>
+                <span class="col-md-1 flex flex-vertical-center flex-space-around no-padding">
                   <a onclick="toggleEntry('#entry-2')"><i class="fa fa-pencil " aria-hidden="true"></i></a>
                   <a onclick="deleteEntry('#entry-2')"><i class="fa fa-trash" aria-hidden="true"></i></a>
                 </span>
               </div>
 
-              <div class="col-md-12 saved-entry flex">
-                <span class="col-md-1 flex flex-vertical-center flex-end">3.</span>
-                <input id="entry-3" type="text" class="col-md-10 entry" value="Added this three" disabled>
-                <span class="col-md-1 flex flex-vertical-center flex-space-around">
+              <div class="col-md-12 saved-entry flex no-padding">
+                <span class="col-md-half flex flex-vertical-center flex-end no-padding">3.</span>
+                <input id="entry-1" type="text" class="col-md-10 entry " value="Added this" disabled>
+                <span class="col-md-1 flex flex-vertical-center flex-space-around no-padding">
                   <a onclick="toggleEntry('#entry-3')"><i class="fa fa-pencil " aria-hidden="true"></i></a>
                   <a onclick="deleteEntry('#entry-3')"><i class="fa fa-trash" aria-hidden="true"></i></a>
                 </span>
               </div>
-            </div>
 
-            <input id="new-entry" type="text" class="form-control" placeholder="What have you done?">
-            <button class="btn btn-default" onclick="toggleEntry('#entry-1')">Enable</button>
-          </div>
+            </div> <!-- End Saved Entries -->
 
-          <div class="col-md-12">
-            <a onclick="stopJob(<?=$_GET['log_id']?>);" class="btn btn-danger col-md-8 col-md-offset-2">Stop</a>
-          </div>
+            <input id="new-entry" type="text" class="form-control row" placeholder="What have you done?">
+            <a onclick="stopJob(<?=$_GET['log_id']?>);" class="btn btn-danger col-md-4 row">Stop</a>
+          </div> <!-- End Description -->
         </div> <!-- End Content -->
       </div> <!--End Row-->
     </div> <!-- End Container Fluid -->
