b8f6ea11d769dcbc7b78faf5b8baec16f3902321
Mark Diez <markediez@gmail.com>
2016-07-22 01:32:06 -0700

    Fix register css

--- register.php
+++ register.php
@@ -7,30 +7,35 @@
     ?>
   </head>
   <body>
-    <div class="container-fluid">
+    <div id="index" class="container-fluid">
 
-      <div id="header" class="row">
-        <div class="col-md-12">
-          <h1>Time Keeper</h1>
+      <div id="header">
+        <div>
         </div>
       </div>
 
-      <div id="register" class="row">
-        <form id="register-form" class="col-md-4">
-          <div class="tooltips col-md-12 no-padding">
+      <div id="register" class="form">
+        <div class="head">
+          <img src="svg/logo.svg" class="head-logo">
+          <span class="head-title">Time Keeper</span>
+          <?php if ($error) {?>
+          <p class="error">Something went wrong</p>
+          <?php } ?>
+        </div>
+        <form id="register-form">
+          <div>
             <input id="username" class="col-md-12 form-control" type="text" placeholder="username" name="username" required>
           </div>
-          <div class="tooltips col-md-12 no-padding">
+          <div>
             <input class="col-md-12 form-control form-item" type="password" placeholder="password" name="password" required>
           </div>
-          <div class="tooltips col-md-12 no-padding">
+          <div>
             <input id="email" class="col-md-12 form-control form-item" type="email" placeholder="email" name="email" required>
           </div>
-          <a id="register-button" class="col-md-4 col-md-offset-8 btn btn-primary form-item" onclick="return postFormSubmit('#register-form', '#register-form .tooltips input', 'http://localhost:8888/db/ajax/register.php')">Register</a>
-          <a class='col-md-4 col-md-offset-8 text-right no-padding' href="/">
+          <a id="register-button" class="btn btn-primary form-item" onclick="return postFormSubmit('#register-form', '#register-form .tooltips input', 'http://localhost:8888/db/ajax/register.php')">Register</a>
+          <a id="register-alter" href="/">
             <span class="small">Already have an account? Login</span>
           </a>
-          <!-- <input type="submit" class="hide"> -->
         </form>
       </div>
     </div>
