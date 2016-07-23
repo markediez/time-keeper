95c1941380cc7229684e8441b27498ca0ef45c29
Mark Diez <markediez@gmail.com>
2016-07-23 12:26:50 -0700

    Using captcha

--- register.php
+++ register.php
@@ -1,10 +1,26 @@
+<?php
+  include('server.php');
+
+  // If user attempts to register, check details
+  if (isset($_POST) && sizeof($_POST) > 0) {
+    $id = login($_POST['username'], $_POST['password']);
+    if ($id > 0) {
+      setSession($_POST, $id);
+      header("Refresh:0");
+    } else {
+      $error = true;
+    }
+  }
+
+  var_dump($KEY);
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <?php
-      include('server.php');
       addHeaders("Time Keeper");
     ?>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
   </head>
   <body>
     <div id="index" class="container-fluid">
@@ -32,6 +48,7 @@
           <div>
             <input id="email" class="col-md-12 form-control form-item" type="email" placeholder="email" name="email" required>
           </div>
+          <div class="g-recaptcha" data-sitekey="<?=$KEY['captcha_form']?>"></div>
           <a id="register-button" class="btn btn-primary form-item" onclick="return postFormSubmit('#register-form', '#register-form .tooltips input', 'http://localhost:8888/db/ajax/register.php')">Register</a>
           <a id="register-alter" href="/">
             <span class="small">Already have an account? Login</span>
