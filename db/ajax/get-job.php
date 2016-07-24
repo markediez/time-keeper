057682f74f566400edc572fd26e99a73849189bb
Mark Diez <markediez@gmail.com>
2016-07-24 02:02:55 -0700

    Refactor get-job to use DBSql

--- db/ajax/get-job.php
+++ db/ajax/get-job.php
@@ -15,15 +15,14 @@ ob_start();
 session_start();
 include('../../server.php');
 checkSession();
-$db = new DBLite();
+$db = new DBSql();
 
 // Redirect to time-progress.php if there is a job in progress
 $query = "SELECT id FROM WorkLog WHERE user_id=:uid and end_time IS NULL OR end_time = ''";
-$query = $db->escapeString($query);
 $statement = $db->prepare($query);
 $statement->bindValue(':uid', $_SESSION['user_id']);
-$res = $statement->execute();
-$row = $res->fetchArray(SQLITE3_ASSOC);
+$statement->execute();
+$row = $statement->fetch(PDO::FETCH_ASSOC);
 $json = array();
 
 if($row) {
@@ -35,13 +34,12 @@ if($row) {
 
 // Otherwise, grab all jobs
 $query = "SELECT id, title FROM Jobs WHERE user_id = :id";
-$query = $db->escapeString($query);
 $statement = $db->prepare($query);
 $statement->bindValue(':id', $_SESSION['user_id']);
-$res = $statement->execute();
+$statement->execute();
 
 
-while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
   array_push($json, $row);
 }
 
