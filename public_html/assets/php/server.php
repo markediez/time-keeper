b0115de3bebfb6652d9ae1e6309509e8fdbf9aa2
Mark Diez <markediez@gmail.com>
2017-05-03 14:36:40 -0700

    Use require and revert last commit

--- public_html/assets/php/server.php
+++ public_html/assets/php/server.php
@@ -18,136 +18,135 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 date_default_timezone_set("America/Los_Angeles");
-include($_SERVER['DOCUMENT_ROOT'] . "/../config/config.php");
-include($_SERVER['DOCUMENT_ROOT'] . "/db/development/database.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/../config/config.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/db/development/database.php");
 
 $config = $_SERVER['DOCUMENT_ROOT'] . "/../config/config.php";
-$data = $_SERVER['DOCUMENT_ROOT'] . "/db/development/database.php"
-echo "<p style='color:white;'> $config </br> $data </p>";
+$data = $_SERVER['DOCUMENT_ROOT'] . "/db/development/database.php";
 
 // http://stackoverflow.com/questions/4503135/php-get-site-url-protocol-http-vs-https
 function getBaseURL() {
 	$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-    $domainName = $_SERVER['HTTP_HOST'].'/';
+	$domainName = $_SERVER['HTTP_HOST'].'/';
 
 	return $protocol.$domainName;
 }
 
 function addHeaders($title) {
-  echo "<title>$title</title>";
-  echo '<meta charset="utf-8">';
-  echo "<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>";
-  echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
-  echo '<link rel="stylesheet" href="' . getBaseURL() . 'vendor/stylesheets/bootstrap.min.css">';
-  echo '<link rel="stylesheet" href="' . getBaseURL() . 'vendor/stylesheets/font-awesome.min.css">';
-  echo '<link rel="stylesheet" href="' . getBaseURL() . 'assets/stylesheets/style.css">';
+	echo "<title>$title</title>";
+	echo '<meta charset="utf-8">';
+	echo "<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>";
+	echo "<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>";
+	echo '<link rel="stylesheet" href="' . getBaseURL() . 'vendor/stylesheets/bootstrap.min.css">';
+	echo '<link rel="stylesheet" href="' . getBaseURL() . 'vendor/stylesheets/font-awesome.min.css">';
+	echo '<link rel="stylesheet" href="' . getBaseURL() . 'assets/stylesheets/style.css">';
 }
 
 function addScripts() {
-  echo '<script type="text/javascript" src="' . getBaseURL() . 'vendor/js/jquery.js"></script>';
-  echo '<script type="text/javascript" src="' . getBaseURL() . 'assets/js/script.js"></script>';
+	echo '<script type="text/javascript" src="' . getBaseURL() . 'vendor/js/jquery.js"></script>';
+	echo '<script type="text/javascript" src="' . getBaseURL() . 'assets/js/script.js"></script>';
 }
 
 function setSession($post, $id = null) {
 
-  $index = 0;
-  $_SESSION[$index++] = $post['user_id'];
-  $_SESSION[$index++] = $post['username'];
-  $_SESSION[$index++] = true;
-  $_SESSION[$index++] = time();
+	$index = 0;
+	$_SESSION[$index++] = $post['user_id'];
+	$_SESSION[$index++] = $post['username'];
+	$_SESSION[$index++] = true;
+	$_SESSION[$index++] = time();
 
-  $_SESSION['user_id'] = $id;
-  $_SESSION['username'] = $post['username'];
-  $_SESSION['valid'] = ($_SESSION['user_id']) ? true : false;
-  $_SESSION['timeout'] = time();
-  $_SESSION['folder'] = getcwd();
+	$_SESSION['user_id'] = $id;
+	$_SESSION['username'] = $post['username'];
+	$_SESSION['valid'] = ($_SESSION['user_id']) ? true : false;
+	$_SESSION['timeout'] = time();
+	$_SESSION['folder'] = getcwd();
 }
 
 function checkSession($redirect = true) {
-  // var_dump($_SESSION);
-  if (array_key_exists('valid', $_SESSION) && $_SESSION['valid']) {
-    return true;
-  } else {
-    if ($redirect) {
-      redirect("index.php");
-    } else {
-      $_SESSION['valid'] = false;
-      return false;
-    }
-  }
+	// var_dump($_SESSION);
+	if (array_key_exists('valid', $_SESSION) && $_SESSION['valid']) {
+		return true;
+	} else {
+		if ($redirect) {
+			redirect("index.php");
+		} else {
+			$_SESSION['valid'] = false;
+			return false;
+		}
+	}
 }
 
 function redirect($url, $statusCode = 303) {
-  header('Location: ' . $url, true, $statusCode);
-  die();
+	header('Location: ' . $url, true, $statusCode);
+	die();
 }
 
 function addPanel() {?>
-  <div id="logo" class="row">
-    <img src="svg/logo.svg" class="img-responsive col-md-4">
-    <span class="col-md-8">Time Keeper</span>
-  </div>
-  <div id="links" class="row">
-    <a onclick="showWork(<?=$_SESSION['user_id']?>);" class="col-md-12 btn btn-panel">Work</a>
-    <a href="time-keeper.php" class="col-md-12 btn btn-panel">Calendar</a>
-    <a href="logout.php" class="col-md-12 btn btn-panel">Logout</a>
-  </div>
-<?php }
+	<div id="logo" class="row">
+		<img src="svg/logo.svg" class="img-responsive col-md-4">
+		<span class="col-md-8">Time Keeper</span>
+	</div>
+	<div id="links" class="row">
+		<a onclick="showWork(<?=$_SESSION['user_id']?>);" class="col-md-12 btn btn-panel">Work</a>
+		<a href="time-keeper.php" class="col-md-12 btn btn-panel">Calendar</a>
+		<a href="logout.php" class="col-md-12 btn btn-panel">Logout</a>
+	</div>
+	<?php }
 
-function login($username, $password) {
-  $db = new DBSql();
+	function login($username, $password) {
+		$db = new DBSql();
 
-  $query = "SELECT * FROM Users WHERE username = :user AND password = :password";
-  $stmt = $db->prepare($query);
-  $stmt->bindParam(':user', $username);
-  $stmt->bindParam(':password', $password);
-  $stmt->execute();
-  $row = $stmt->fetch(PDO::FETCH_ASSOC);
-  $db = null;
-  $stmt = null;
-  return $row['id'];
-}
+		$query = "SELECT * FROM Users WHERE username = :user AND password = :password";
+		$stmt = $db->prepare($query);
+		$stmt->bindParam(':user', $username);
+		$stmt->bindParam(':password', $password);
+		$stmt->execute();
+		$row = $stmt->fetch(PDO::FETCH_ASSOC);
+		$db = null;
+		$stmt = null;
+		return $row['id'];
+	}
 
-function register($username, $password, $email) {
-  $db = new DBSql();
-  $currDateTime = date('Y-m-d H:i:s');
+	function register($username, $password, $email) {
+		$db = new DBSql();
+		$currDateTime = date('Y-m-d H:i:s');
 
-  $statement = $db->prepare("INSERT INTO Users (role_id, username, password, email, created_at, updated_at)
-            VALUES (2, :username, :password, :email, :start_time, :end_time)");
+		$statement = $db->prepare("INSERT INTO Users (role_id, username, password, email, created_at, updated_at)
+		VALUES (2, :username, :password, :email, :start_time, :end_time)");
 
-  $statement->bindParam(':username', $username);
-  $statement->bindParam(':password', $password);
-  $statement->bindParam(':email', $email);
-  $statement->bindParam(':start_time', $currDateTime);
-  $statement->bindParam(':end_time', $currDateTime);
-  try {
-    $statement->execute();
-    return $db->lastInsertId();
-  } catch(PDOException $e) {
-    return $e->getMessage();
-  }
-}
+		$statement->bindParam(':username', $username);
+		$statement->bindParam(':password', $password);
+		$statement->bindParam(':email', $email);
+		$statement->bindParam(':start_time', $currDateTime);
+		$statement->bindParam(':end_time', $currDateTime);
+		try {
+			$statement->execute();
+			return $db->lastInsertId();
+		} catch(PDOException $e) {
+			return $e->getMessage();
+		}
+	}
 
-function verifyCaptcha($response) {
-  // http://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php
-  $url = 'https://www.google.com/recaptcha/api/siteverify';
-  $data = array('secret' => $GLOBALS['KEY']['captcha_secret'],
-                'response' => $response);
+	function verifyCaptcha($response) {
+		// http://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php
+		$url = 'https://www.google.com/recaptcha/api/siteverify';
+		$data = array('secret' => $GLOBALS['KEY']['captcha_secret'],
+		'response' => $response);
 
-  // use key 'http' even if you send the request to https://...
-  $options = array(
-      'http' => array(
-          'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-          'method'  => 'POST',
-          'content' => http_build_query($data)
-      )
-  );
-  $context  = stream_context_create($options);
-  $result = file_get_contents($url, false, $context);
-  if ($result === FALSE) { /* Handle error */ }
+		// use key 'http' even if you send the request to https://...
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => http_build_query($data)
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+	if ($result === FALSE) { /* Handle error */ }
 
-  $result = json_decode($result);
-  return $result->success;
+	$result = json_decode($result);
+	return $result->success;
 }
 
 ?>
